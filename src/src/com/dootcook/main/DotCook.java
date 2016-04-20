4a964843c9add0e98ea1a569410e1fc4a7c45a02
Christian Peña <christian.pena.farias@hotmail.com>
2016-04-20 18:35:29 -0300

    First Class
    Creación de proyecto eclipse

---
+++ src/src/com/dootcook/main/DotCook.java
@@ -0,0 +1,17 @@
+package com.dootcook.main;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class DotCook extends Application{
+	
+	@Override
+	public void start(Stage stage){
+		
+	}
+	
+	public static void main(String[]args){
+		launch(args);
+	}
+
+}
